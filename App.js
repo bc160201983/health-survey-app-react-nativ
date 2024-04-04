7fdf814696d3d49edf04272607d47617ee7b1da5
Muhammad Bilal <softnacuk@gmail.com>
2024-04-04 23:13:05 +0100

    error fixed

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from "expo-status-bar";
-import * as Notifications from "expo-notifications";
 
 import { Alert, StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
@@ -14,57 +13,48 @@ import { SurveyProvider } from "./surveyContext";
 import { useEffect, useRef } from "react";
 
 const Stack = createNativeStackNavigator();
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-  }),
-});
 
 export default function App() {
-  const responseListener = useRef();
+  //   let token;
 
-  async function registerForPushNotificationsAsync() {
-    let token;
+  //   const { status: existingStatus } =
+  //     await Notifications.getPermissionsAsync();
+  //   let finalStatus = existingStatus;
 
-    const { status: existingStatus } =
-      await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
+  //   if (existingStatus !== "granted") {
+  //     const { status } = await Notifications.requestPermissionsAsync();
+  //     finalStatus = status;
+  //   }
+  //   if (finalStatus !== "granted") {
+  //     alert("Failed to get push token for push notification!");
+  //     return;
+  //   }
+  //   token = (await Notifications.getExpoPushTokenAsync()).data;
+  //   console.log(token);
 
-    if (existingStatus !== "granted") {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== "granted") {
-      alert("Failed to get push token for push notification!");
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log(token);
+  //   return token;
+  // }
+  // Notifications.setNotificationHandler({
+  //   handleNotification: async () => ({
+  //     shouldShowAlert: true,
+  //   }),
+  // });
 
-    return token;
-  }
+  // useEffect(() => {
+  //   registerForPushNotificationsAsync();
+  //   // // Works when app is foregrounded, backgrounded, or killed
+  //   // responseListener.current =
+  //   //   Notifications.addNotificationResponseReceivedListener((response) => {
+  //   //     console.log("--- notification tapped ---");
+  //   //     console.log(response);
+  //   //     console.log("------");
+  //   //   });
 
-  Notifications.setNotificationHandler({
-    handleNotification: async () => ({
-      shouldShowAlert: true,
-    }),
-  });
-
-  useEffect(() => {
-    registerForPushNotificationsAsync();
-    // Works when app is foregrounded, backgrounded, or killed
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((response) => {
-        console.log("--- notification tapped ---");
-        console.log(response);
-        console.log("------");
-      });
-
-    // Unsubscribe from events
-    return () => {
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
+  //   // // Unsubscribe from events
+  //   // return () => {
+  //   //   Notifications.removeNotificationSubscription(responseListener.current);
+  //   //};
+  // }, []);
   return (
     <DeviceUUIDProvider>
       <SurveyProvider>
--- context.js
+++ context.js
@@ -1,7 +1,6 @@
 // DeviceUUIDProvider.js
 import React, { createContext, useContext, useState, useEffect } from "react";
 import * as SecureStore from "expo-secure-store";
-import * as Crypto from "expo-crypto";
 
 const DeviceUUIDContext = createContext("");
 
